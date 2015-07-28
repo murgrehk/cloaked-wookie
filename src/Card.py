c0a72fa004e3cc1a36cea4a02533015bd9ba4090
Seth Ebner <murgrehk@gmail.com>
2015-07-28 00:45:53 -0500

    Added persistent card statistics tracking
    The user is now presented with his/her history of the played card.

--- src/Card.py
+++ src/Card.py
@@ -4,9 +4,10 @@
 
 class Card(object):
 
-    def __init__(self, clue, answers, correct=0, incorrect=0, peeks=0):
+    def __init__(self, clue, answers, correct=0, incorrect=0, peeks=0, sessions_since_last_seen=0):
         self.clue = clue
         self.answers = answers
         self.correct = correct
         self.incorrect = incorrect
+        self.sessions_since_last_seen = sessions_since_last_seen
         self.peeks = peeks
