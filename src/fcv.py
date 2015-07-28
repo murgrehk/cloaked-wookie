bcd077022a1dd9ff2cd2378f216c89bc8115dc92
Seth Ebner <murgrehk@gmail.com>
2015-07-27 23:31:06 -0500

    Moved files to src directory

--- src/fcv.py
+++ src/fcv.py
@@ -125,9 +125,9 @@ def print_results(stats, session_stats):
 
 def main():
 	deck_name = get_deck_name()
-	deck_filename = './Decks/{}.txt'.format(deck_name)
-	pickle_filename = './Pickles/{}_pickled.txt'.format(deck_name)
-	results_filename = './Results/{}_results.txt'.format(deck_name)
+	deck_filename = '../Decks/{}.txt'.format(deck_name)
+	pickle_filename = '../Pickles/{}_pickled.txt'.format(deck_name)
+	results_filename = '../Results/{}_results.txt'.format(deck_name)
 
 	[deck, common_cards, new_cards, removed_cards] = load_and_update_deck( pickle_filename, deck_filename )
 
